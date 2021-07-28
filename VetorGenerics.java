d4c989c215831258347c0667cc08dcb67a784dbb
gabriela-olivrs <82829835+gabriela-olivrs@users.noreply.github.com>
2021-07-28 20:37:48 -0300

    Add files via upload

---
+++ VetorGenerics.java
@@ -0,0 +1,98 @@
+package estrutura.de.dados.vetores;
+
+public class VetorGenerics<T> {
+
+    private T[] elementos;
+    private int tamanho;
+
+    public VetorGenerics(int capacidade) {
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public boolean adiciona(T elemento) {
+
+        if (this.tamanho < this.elementos.length) {
+
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean adiciona(int posicao, T elemento) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        for (int i = this.tamanho - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return false;
+
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            T[] elementosNovos = (T[]) new Object[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+
+    }
+
+    public T busca(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        return this.elementos[posicao];
+    }
+
+    public int busca(T elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i].equals(elemento)) {
+                return i;
+
+            }
+
+        }
+        return -1;
+    }
+
+    public void remove(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+
+        }
+        this.tamanho--;
+
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(",");
+        }
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+        s.append("]");
+        return s.toString();
+    }
+
+}
